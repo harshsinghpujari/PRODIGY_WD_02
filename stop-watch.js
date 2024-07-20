a1b1b8c738ba65d75c2b3026c05e9a822735200f
Harsh singh <mastermindharsh077@gmail.com>
2024-07-20 14:25:33 +0530

    Initial commit

---
+++ stop-watch.js
@@ -0,0 +1,72 @@
+var isStop = true;
+
+var sec = 0;
+var min = 0;
+var hr = 0;
+
+
+function start () 
+{
+  if(isStop == true) 
+  {
+    isStop = false;
+
+    timer();
+  }
+}
+
+function timer () 
+{
+
+  sec = parseInt(sec);
+  min = parseInt(min);
+  hr = parseInt(hr);
+
+  if(isStop == false) {
+    sec++;
+
+    if(sec==60) {
+      sec = 0;
+      min++;
+    }
+
+    if(min==60) {
+      min = 0;
+      hr++;
+    }  
+    
+    if(sec<10) {
+      sec = "0" + sec;
+    }
+
+    if(min<10) {
+      min = "0"+min;
+    }
+
+    if(hr<10) {
+      hr = "0"+hr;
+    }
+
+
+
+    
+    
+    clock.innerHTML = hr + " : " + min + " : " + sec;
+    setTimeout("timer()", 1000);
+
+  }
+}
+
+function stop() {
+  isStop = true;
+}
+
+function reset() {
+  isStop = true;
+
+  sec = 0;
+  min = 0;
+  hr = 0;
+
+  clock.innerHTML = "00 : 00 : 00"
+}
